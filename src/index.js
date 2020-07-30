ffafd5dc8d5fee364e9499e101efb58516a2983d
Andrey Neumyvannyi <andreyneumuvanny@gmail.com>
2020-07-30 14:20:17 +0400

    select: date picker plugin. Selected FLATPICKR. start: styling the plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 //import "./"
 
 // JS
-import "./components/range-slider/range-slider.js";
+import flatpickr from "flatpickr";
+const Russian = require("flatpickr/dist/l10n/ru.js").default.ru;
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+flatpickr(".fp_range", {
+  locale: Russian,
+  mode: "range",
+  plugins: [new rangePlugin({ input: "#secondRangeInput" })],
+});
 
 // SCSS
 import "./style.scss";
 
 //CSS
+import "flatpickr/dist/flatpickr.min.css";
 import "nouislider/distribute/nouislider.min.css";
