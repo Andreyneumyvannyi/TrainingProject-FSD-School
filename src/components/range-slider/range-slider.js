0098c0686b8632a53f13f3f0530ae6096fa6f83d
Andrey Neumyvannyi <andreyneumuvanny@gmail.com>
2020-07-31 18:36:30 +0400

    add: page headerFooter. fix: dropdown-date, range-slider. start: calendar and slider room card

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,32 +1,40 @@
-import noUiSlider from "nouislider";
+if (
+  $(document)
+    .find("title")
+    .text() === "FormElements"
+) {
+  const noUiSlider = require("nouislider/distribute/nouislider");
 
-var skipSlider = document.getElementById("skipstep");
-let iconRuble = "₽";
+  let skipSlider = document.getElementById("skipstep");
+  let iconRuble = "₽";
 
-noUiSlider.create(skipSlider, {
-  connect: true,
-  range: {
-    min: 0,
-    "10%": 1000,
-    "20%": 2000,
-    "30%": 3000,
-    "40%": 4000,
-    "50%": 5000,
-    "60%": 6000,
-    "70%": 7000,
-    "80%": 8000,
-    "90%": 9000,
-    max: 10000,
-  },
-  snap: true,
-  start: [5000, 9000],
-});
+  noUiSlider.create(skipSlider, {
+    connect: true,
+    range: {
+      min: 0,
+      "10%": 1000,
+      "20%": 2000,
+      "30%": 3000,
+      "40%": 4000,
+      "50%": 5000,
+      "60%": 6000,
+      "70%": 7000,
+      "80%": 8000,
+      "90%": 9000,
+      max: 10000,
+    },
+    snap: true,
+    start: [5000, 9000],
+  });
 
-var skipValues = [
-  document.getElementById("range-slider__price-lower"),
-  document.getElementById("range-slider__price-upper"),
-];
+  var skipValues = [
+    document.getElementById("range-slider__price-lower"),
+    document.getElementById("range-slider__price-upper"),
+  ];
 
-skipSlider.noUiSlider.on("update", function(values, handle) {
-  skipValues[handle].innerHTML = String(Math.round(values[handle]) + iconRuble);
-});
+  skipSlider.noUiSlider.on("update", function(values, handle) {
+    skipValues[handle].innerHTML = String(
+      Math.round(values[handle]) + iconRuble
+    );
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 //import "./"
 
 // JS
+import "./components/range-slider/range-slider";
+
 import flatpickr from "flatpickr";
 const Russian = require("flatpickr/dist/l10n/ru.js").default.ru;
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
-flatpickr(".fp_range", {
+flatpickr(".dropdown-date__range", {
   locale: Russian,
   mode: "range",
   plugins: [new rangePlugin({ input: "#secondRangeInput" })],
