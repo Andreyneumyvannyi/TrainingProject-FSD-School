6d34322609df83b468cfd7a53a62198936770a46
Andrey Neumyvannyi <andreyneumuvanny@gmail.com>
2020-08-11 18:13:15 +0400

    fix: calendar

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "./components/dropdown/dropdown-facilities/dropdown-facilities";
 import "./components/checkbox/checkbox-list/checkbox-list";
 
 // Calendar
-import flatpickr from "flatpickr";
+/*import flatpickr from "flatpickr";
 const Russian = require("flatpickr/dist/l10n/ru.js").default.ru;
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
@@ -20,6 +20,38 @@ flatpickr(".dropdown-date__range", {
   locale: Russian,
   mode: "range",
   plugins: [new rangePlugin({ input: "#secondRangeInput" })],
+});
+*/
+
+import daterangepicker from "daterangepicker";
+$(".dropdown-date__range").daterangepicker({
+  linkedCalendars: false,
+  opens: "center",
+  locale: {
+    format: "DD.MM.YYYY",
+    separator: " - ",
+    applyLabel: "Применить",
+    cancelLabel: "Очистить",
+    fromLabel: "From",
+    toLabel: "To",
+    customRangeLabel: "Custom",
+    daysOfWeek: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    monthNames: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    firstDay: 1,
+  },
 });
 
 // SCSS
@@ -30,3 +62,4 @@ import "flatpickr/dist/flatpickr.min.css";
 import "nouislider/distribute/nouislider.min.css";
 import "slick-carousel/slick/slick.css";
 import "jquery-bar-rating/dist/themes/css-stars.css";
+import "daterangepicker/daterangepicker.css";
